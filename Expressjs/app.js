5736bb8dae3f2aab0cb2ccb3a3edc797cd222e24
akash rathour <akkurathour2@gmail>
2025-03-14 21:17:50 +0530

    added BodyParser

--- Expressjs/app.js
+++ Expressjs/app.js
@@ -1,12 +1,27 @@
-const http=require('http')
-const express=require('express')
-const app=express()
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
 
-app.use('/add-product',(req,res,next)=>{
-res.send('<h1>This is Add Product Page<h1>')
-})
-app.use('/',(req,res,next)=>{
-    console.log("Another Moddleware")
-    res.send('<h1>Hello From Express js</h1>')
-})
-app.listen(3000,()=>console.log('server is running at 3000'))
+app.use(bodyParser.urlencoded({ extended: false })); // Use built-in body parser
+
+app.use('/add-user', (req, res, next) => {
+    res.send(`<form method="POST" action='/user'>
+        <input type="text" name="user" placeholder="Enter text"/>
+        <button type="submit">Submit</button>
+    </form>`);
+});
+
+app.use('/user', (req, res) => {
+    console.log(req.body.user); 
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    if (req.url === '/favicon.ico') {
+        return res.status(204).end();
+    }
+    console.log('In path /');
+    res.send('<h1>Home Page</h1>');
+});
+
+app.listen(3000, () => console.log('Server is running at 3000'));
