b3723c71a3331506b96b47b0fb6f5354eaaa5dd5
akash rathour <akkurathour2@gmail>
2025-03-14 18:24:52 +0530

    added

--- Expressjs/app.js
+++ Expressjs/app.js
@@ -2,5 +2,11 @@ const http=require('http')
 const express=require('express')
 const app=express()
 
-const server=http.createServer(app)
-server.listen(3000,()=>console.log('your port is running at 3000'))
+app.use('/add-product',(req,res,next)=>{
+res.send('<h1>This is Add Product Page<h1>')
+})
+app.use('/',(req,res,next)=>{
+    console.log("Another Moddleware")
+    res.send('<h1>Hello From Express js</h1>')
+})
+app.listen(3000,()=>console.log('server is running at 3000'))
