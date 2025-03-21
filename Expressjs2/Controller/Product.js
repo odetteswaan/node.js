f431921fff19557bb946863cb615e5555c59f853
akash rathour <akkurathour2@gmail>
2025-03-21 20:22:26 +0530

    added models

--- Expressjs2/Controller/Product.js
+++ Expressjs2/Controller/Product.js
@@ -1,4 +1,4 @@
-const products=[]
+const Product=require('../models/Product')
 
 exports.addProducts=(req, res, next) => {
     res.render('add-product', {
@@ -11,11 +11,14 @@ exports.addProducts=(req, res, next) => {
   }
 
 exports.addNewProducts=(req, res, next) => {
-  products.push({ title: req.body.title });
+const product=new Product(req.body.title)
+product.save()
+
   res.redirect('/');
 }
 
 exports.getProducts=(req, res, next) => {
+  const products=Product.fetchAll()
   res.render('shop', {
     prods: products,
     pageTitle: 'Shop',
