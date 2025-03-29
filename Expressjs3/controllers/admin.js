8dbe4434bc00a6b15d8d63bad8b3ae6602a31d42
akash rathour <akkurathour2@gmail>
2025-03-29 16:40:22 +0530

    added Edit product functionality

--- Expressjs3/controllers/admin.js
+++ Expressjs3/controllers/admin.js
@@ -13,7 +13,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null,title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
@@ -23,7 +23,6 @@ exports.getEditProduct = (req, res, next) => {
     if(!product){
       return res.redirect('/')
     }
-    console.log(product)
     res.render('admin/edit-product', {
       pageTitle: 'Edit Product',
       path: '/admin/Edit-product',
@@ -32,7 +31,6 @@ exports.getEditProduct = (req, res, next) => {
     });
 
   })
-  // console.log(prodId)
 };
 
 exports.getProducts = (req, res, next) => {
@@ -45,3 +43,14 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
+exports.postEditProduct=(req,res,next)=>{
+  const prodId=req.body.productId;
+  const updatedTitle=req.body.title;
+  const updatedPrice=req.body.price;
+  const updatedDescription=req.body.description;
+  const updatedImageUrl=req.body.imageUrl;
+  const updatedProduct=new Product(prodId,updatedTitle,updatedImageUrl,updatedDescription,updatedPrice)
+  updatedProduct.save()
+  res.redirect('/admin/products')
+
+}
--- Expressjs3/models/product.js
+++ Expressjs3/models/product.js
@@ -18,7 +18,8 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id,title, imageUrl, description, price) {
+    this.id=id
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -27,11 +28,22 @@ module.exports = class Product {
 
   save() {
     getProductsFromFile(products => {
-      this.id=Math.random().toString()
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
-      });
+      if(this.id){
+         const existingProductIndex=products.findIndex(prod=>prod.id===this.id);
+         const updatedProducts=[...products];
+         updatedProducts[existingProductIndex]=this;
+         fs.writeFile(p,JSON.stringify(updatedProducts),err=>{
+          console.log(err)
+         })
+      }
+      else{
+
+        this.id=Math.random().toString()
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), err => {
+          console.log(err);
+        });
+      }
     });
   }
 
